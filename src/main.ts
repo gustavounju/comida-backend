bf9b86d150f7e2e73390a6a6d030897dc6205c54
DESKTOP-9GSPGFF\Gustavo <gustavoeliasm@gmail.com>
2025-07-09 15:10:07 -0300

    se instalo express-session

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Configurar sesiones
+  app.use(
+    session({
+      secret: '12345', // Cambia esto por un valor seguro
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 3600000 }, // 1 hora de duración
+    }),
+  );
+
+  // Configurar Swagger
   const config = new DocumentBuilder()
     .setTitle('Comida Backend API')
     .setDescription('API para la gestión de una tienda de comidas online')
